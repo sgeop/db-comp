ebcd0d410ff2f6a12face003775df5690d571820
Sean Pietz <spietz@handy.com>
2018-02-07 02:35:32 -0500

    add queries

--- default.nix
+++ default.nix
@@ -1,10 +1,14 @@
-{ mkDerivation, base, beam-core, opaleye, stdenv }:
+{ mkDerivation, base, beam-core, beam-sqlite, mtl, opaleye
+, sqlite-simple, stdenv, text
+}:
 mkDerivation {
   pname = "db-comp";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base beam-core opaleye ];
+  executableHaskellDepends = [
+    base beam-core beam-sqlite mtl opaleye sqlite-simple text
+  ];
   license = stdenv.lib.licenses.bsd3;
 }
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DeriveGeneric #-}
@@ -7,27 +8,96 @@
 
 module Main where
 
+import Control.Monad.Reader
+
 import Database.Beam
-import Database.Beam.Backend.Sqlite3
+import Database.Beam.Sqlite
+import Database.SQLite.Simple
 
-import Data.Text (Text)
+import Data.Text(Text)
 
--- data UserT f = User
---   { _userEmail     :: Columnar f Text
---   , _userFirstName :: Columnar f Text
---   , _userLastName  :: Columnar f Text
---   , _userPassword  :: Columnar f Text }
---    deriving Generic
---
--- type User = UserT Identity
--- deriving instance Show User
+data UserT f = User
+  { _userEmail     :: Columnar f Text
+  , _userFirstName :: Columnar f Text
+  , _userLastName  :: Columnar f Text
+  , _userPassword  :: Columnar f Text }
+   deriving Generic
 
--- instance Table UserT where
---   data PrimaryKey UserT f = UserId (Columnar f Text) deriving Generic
---   primaryKey = UserId . _userEmail
---
--- type UserId = PrimaryKey UserT Identity
--- deriving instance Show UserId
+type User = UserT Identity
+type UserId = PrimaryKey UserT Identity
 
- main :: IO ()
- main = putStrLn "Hello, Haskell!"
+deriving instance Show User
+deriving instance Eq User
+
+instance Beamable UserT
+
+instance Table UserT where
+    data PrimaryKey UserT f = UserId (Columnar f Text) deriving Generic
+    primaryKey = UserId . _userEmail
+instance Beamable (PrimaryKey UserT)
+
+data ShoppingCartDb f = ShoppingCartDb
+ { _shoppingCartUsers :: f (TableEntity UserT)
+ } deriving Generic
+
+instance Database ShoppingCartDb
+
+shoppingCartDb :: DatabaseSettings be ShoppingCartDb
+shoppingCartDb = defaultDbSettings
+
+insertUsers :: Connection -> IO ()
+insertUsers conn = do
+  withDatabase conn $ runInsert $
+    insert (_shoppingCartUsers shoppingCartDb) $
+      insertValues
+        [ User
+          "james@example.com"
+          "James"
+          "Smith"
+          "b4cc344d25a2efe540adbf2678e2304c"
+        , User
+          "betty@example.com"
+          "Betty"
+          "Jones"
+          "82b054bd83ffad9b6cf8bdb98ce3cc2f"
+        , User
+          "sam@example.com"
+          "Sam"
+          "Taylor"
+          "332532dcfaa1cbf61e2a266bd723612c"
+        , User
+          "sam@jely.com"
+          "Sam"
+          "Jely"
+          "332532dcfaa1cbf61e2a266bd723612c"
+        , User
+          "james@oreily.com"
+          "James"
+          "O'Reily"
+          "b4cc344d25a2efe540adbf2678e2304c"
+        ]
+
+type DB a = ReaderT Connection IO a
+
+numUsersByName :: Connection -> IO [(Text, Int)]
+numUsersByName conn =
+  withDatabase conn $ runSelectReturningList $ select query
+  where
+    query =
+      aggregate_ (\u -> (group_ (_userFirstName u), countAll_)) $
+        all_ (_shoppingCartUsers shoppingCartDb)
+
+numUsers :: Connection -> IO (Maybe Int)
+numUsers conn =
+  withDatabase conn $ runSelectReturningOne $ select $
+    aggregate_ (\_ -> countAll_) (all_ (_shoppingCartUsers shoppingCartDb))
+
+main :: IO ()
+main = do
+  conn <- open "shoppingcart1.db"
+  count <- numUsers conn
+  case count of
+    Just 0 -> insertUsers conn
+    _ -> pure ()
+  byName <- numUsersByName conn
+  mapM_ (liftIO . putStrLn . show) byName
